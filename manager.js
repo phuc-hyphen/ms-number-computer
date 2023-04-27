87318431c4396afaf89667b434081faa45c80f38
Phuc LE HUU <cotiphuong001@gmail.com>
2023-04-27 19:42:25 +0200

    exceptions handler

--- manager.js
+++ manager.js
@@ -1,30 +1,30 @@
-const MM = require('ms-manager');  
-let config = require(`./config.json`) || {};  
+const MM = require('ms-manager');
+let config = require(`./config.json`) || {};
 
-MM.init(config, (err, serviceInfo) => {  
-    if (err) {  
-        return console.error(err);  
+MM.init(config, (err, serviceInfo) => {
+    if (err) {
+        return console.error(err);
     }
-    
+
     /**  
     * Our micro-service is now up. 
     * */
-    console.log('#Micro-service UP#');  
-    
+    console.log('#Micro-service UP#');
+
     /**
     * You can now subscribe to specific message
     */
-    MM.subscribe('add', (bdy, msg) => {  
+    MM.subscribe('add', (bdy, msg) => {
         /**
         * TODO: Uncomment when operationnal
         **/
-        // const computer = require('./computer');
-        // try {
-        //     const result = computer.add(bdy.a, bdy.b);
-        //     msg.reply({ result });
-        // } catch (err) {
-        //     console.error(err);
-        //     return msg.replyErr(err);
-        // }
+        const computer = require('./computer');
+        try {
+            const result = computer.add(bdy.a, bdy.b);
+            msg.reply({ result });
+        } catch (err) {
+            console.error(err);
+            return msg.replyErr(err);
+        }
     });
 });
